8eb676433e7890d5fdda53cc57ee59d05679403f
LC6666 <sophine_lc@163.com>
2019-04-24 19:07:27 +0800

    test

---
+++ api/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+import requests
+
+# Create your tests here.
+
+requests.request()
---
+++ api/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render_to_response
+from django.http.response import HttpResponse
+import json
+
+# Create your views here.
+
+def login(request):
+    if request.method=='POST':
+        result = {}
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        result['username'] = username
+        result['password'] = password
+        result = json.dumps(result)
+        return HttpResponse(result, content_type='application/json;charset=utf-8')
+    # if request.method=='GET':
+    #     result = {}
+    #     username = request.GET.get('username')
+    #     password = request.GET.get('password')
+    #     print(username,password)
+    #     result['username']=username
+    #     result['password'] =password
+    #     result = json.dumps(result)
+    #     return HttpResponse(result,content_type='application/json;charset=utf-8')
+    else:
+        return render_to_response('login.html')
