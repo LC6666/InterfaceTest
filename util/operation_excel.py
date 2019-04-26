16e4c7c7a1d217540490f80ad9667ef7a5a1cb00
LC6666 <sophine_lc@163.com>
2019-04-26 12:40:39 +0800

    test

--- util/operation_excel.py
+++ util/operation_excel.py
@@ -8,3 +8,30 @@ tables = data.sheet_by_index(0)
 print(tables.nrows)
 print(tables.cell(0,3))
 
+
+class OperationExcel:
+    def __init__(self,file_name=None,sheet_name=None):
+        if file_name:
+            self.file_name = file_name
+            self.sheet_name = sheet_name
+
+        else:
+            self.file_name='../case/AutoLink_api_tests.xlsx'
+            self.sheet_name='Sheet1'
+        self.data = self.get_data()
+
+    def get_data(self):
+        data = xlrd.open_workbook(self.file_name)
+        tables = data.sheet_by_name(self.sheet_name)
+        return tables
+
+    def get_rowNum(self):
+        tables = self.data
+        return tables.nrows
+
+    def get_cellVale(self,rowx,clox):
+        return self.data.cell(rowx,clox)
+
+if __name__ == '__main__':
+    openers = OperationExcel()
+    print(openers.get_rowNum())
---
+++ util/peration_json.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+__author__ = "豆豆嗯嗯"
+
+import json
+
+# fp = open("../case/case.json")
+# data = json.load(fp)
+# print(data['user1'])
+# fp.close()
+
+
+class OperationJson:
+    def __init__(self):
+        self.data = self.read_data()
+
+    # 读取json文件
+    def read_data(self):
+        with open("../case/case.json") as fp:
+            data = json.load(fp)
+            return data
+
+    def get_data(self,id):
+        return self.data[id]
+
+
+if __name__ == '__main__':
+    jsondata = OperationJson()
+    jsondata.get_data('user1')
